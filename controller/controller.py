5888959cb8f05ded1733c04b541648d174c657be
Vladimir <fok.vov@gmail.com>
2023-02-13 14:28:06 +0300

    add logics to controller

---
+++ controller/controller.py
@@ -0,0 +1,19 @@
+def button_click():
+    point = -1
+    while point != 0:
+        # menu interface
+        # point =
+        if point == 1:
+            pass  # all list
+        elif point == 2:
+            pass  # search
+        elif point == 3:
+            pass  # add
+        elif point == 4:
+            pass  # edit
+        elif point == 5:
+            pass  # delete
+        if point != 0:
+            input('Для возврата в главное меню введите любой символ.\n')
+    else:
+        print('Завершение работы программы.')

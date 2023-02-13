335a75a44919803c215b81591991401e8bbdfdf0
Vladimir <fok.vov@gmail.com>
2023-02-13 16:48:10 +0300

    create reader_from_file

--- util/csv_write_reader.py
+++ util/csv_write_reader.py
@@ -6,3 +6,11 @@ def write_to_file(lst: list):
         writer = csv.writer(f, delimiter=';')
         writer.writerows(lst)
 
+
+def read_from_file():
+    lst = list()
+    with open("../data.csv", 'r', encoding="UTF=8") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            lst.append(row)
+    return lst
